79b5716f499e34fd9772a5c62bc4c3cc6f045d32
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-16 14:59:50 +0200

    create inputs in action.yml and index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Testing...")
+import {getInput, setFailed} from "@actions/core";
+import {context, getOctokit} from "@actions/github";
+
+async function run(){
+    const bpmnFiles = getInput("bpmn-files-path");
+    const customRules = getInput("custom-rules-folder");
+    const bpmnlintrc = getInput("bpmnlintrc-path");
+    // const token = getInput("gh-token");
+
+
+    // const octokitClient = getOctokit(token);
+
+    try {
+
+        console.log(`BPMN files: ${bpmnFiles}`);
+        console.log(`Custom rules: ${customRules}`);
+        console.log(`bpmnlintrc file: ${bpmnlintrc}`);
+
+    } catch (error) {
+        setFailed((error as Error)?.message ?? "Unknown error");
+    }
+
+
+}
