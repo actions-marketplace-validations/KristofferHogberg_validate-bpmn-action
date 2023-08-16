a35537bd4bf558e8fae87ab55542b32b9ffe8149
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-16 16:30:18 +0200

    test read file again

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,22 @@ import * as path from 'path';
 import {getInput, setFailed} from "@actions/core";
 
 
-async function run(){
-
+async function run() {
     const customRules = getInput("custom-rules-folder");
     const bpmnFiles = getInput("bpmn-files-path");
     const bpmnlintrc = getInput("bpmnlintrc-path");
 
-    const fs = require('fs');
-    const content = fs.readdirSync(bpmnFiles, 'utf-8');
-    console.log(content);
-
     try {
-
         console.log(`BPMN files: ${bpmnFiles}`);
         console.log(`Custom rules: ${customRules}`);
         console.log(`bpmnlintrc file: ${bpmnlintrc}`);
 
-
-        const dirContents = fs.readdirSync(__dirname);
+        const dirContents = fs.readdirSync(bpmnFiles, 'utf-8'); // Read directory contents
         console.log(dirContents);
 
     } catch (error) {
         setFailed((error as Error)?.message ?? "Unknown error");
     }
-
 }
 
 
