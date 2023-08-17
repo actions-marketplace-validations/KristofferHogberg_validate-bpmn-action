fa632e6f6303d5435fd60ce3853fbe4ac286277f
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-17 11:10:28 +0200

    bpmnlint with files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { getInput, setFailed } from "@actions/core";
-import {execSync} from "child_process";
+import { execSync } from "child_process"; // Import execSync for running commands
 
 async function run() {
     const customRules = getInput("custom-rules-folder");
@@ -9,36 +9,29 @@ async function run() {
     const bpmnlintrc = getInput("bpmnlintrc-path");
 
     try {
-
-        console.log("TESTING TESTING");
         const dirContents = fs.readdirSync(bpmnFiles, 'utf-8');
 
-        const result = execSync("npx bpmnlint --version", {
-            encoding: "utf-8"
-        });
-        console.log("bpmnlint version:", result);
+        // Run bpmnlint command for each file
+        for (const file of dirContents) {
+            if (file.endsWith(".bpmn")) {
+                const filePath = path.join(bpmnFiles, file);
 
-        // for (const file of dirContents) {
-        //     if (file.endsWith(".bpmn")) {
-        //         const filePath = path.join(bpmnFiles, file);
-        //
-        //         try {
-        //             const result = execSync(`bpmnlint "${filePath}"`, {
-        //                 encoding: "utf-8",
-        //                 stdio: "pipe"
-        //             });
-        //
-        //             if (result.trim() === "") {
-        //                 console.log(`No errors found in ${file}`);
-        //             } else {
-        //                 console.log(`Errors found in ${file}:`);
-        //                 console.log(result);
-        //             }
-        //         } catch (error) {
-        //             console.log(`Errors found in ${file}:`);
-        //         }
-        //     }
-        // }
+                try {
+                    const result = execSync(`npx bpmnlint "${filePath}"`, {
+                        encoding: "utf-8"
+                    });
+
+                    if (result.trim() === "") {
+                        console.log(`No errors found in ${file}`);
+                    } else {
+                        console.log(`Errors found in ${file}:`);
+                        console.log(result);
+                    }
+                } catch (error) {
+                    console.log(`Errors found in ${file}:`);
+                }
+            }
+        }
     } catch (error) {
         setFailed((error as Error)?.message ?? "Unknown error");
     }
