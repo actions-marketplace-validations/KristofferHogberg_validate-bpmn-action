f12e37ec3df08db045ce30ee14faab9d3346d12b
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-17 13:14:01 +0200

    install bpmnlint via script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { getInput, setFailed } from "@actions/core";
-import {execSync} from "child_process";
+import { execSync } from "child_process";
 
 async function run() {
     const customRules = getInput("custom-rules-folder");
@@ -9,24 +9,31 @@ async function run() {
     const bpmnlintrc = getInput("bpmnlintrc-path");
 
     try {
-        // console.log(`BPMN files: ${bpmnFiles}`);
-        // console.log(`Custom rules: ${customRules}`);
-        // console.log(`bpmnlintrc file: ${bpmnlintrc}`);
+        // Copy or create .bpmnlintrc in the same directory as bpmn files
+        const bpmnlintConfigPath = path.join(bpmnFiles, '.bpmnlintrc');
+        if (!fs.existsSync(bpmnlintConfigPath)) {
+            const bpmnlintConfigContent = {
+                extends: "bpmnlint:recommended"
+            };
+            fs.writeFileSync(bpmnlintConfigPath, JSON.stringify(bpmnlintConfigContent, null, 2));
+        }
 
-        const result = execSync("npx bpmnlint --version", {
+        console.log(bpmnlintConfigPath);
+
+        const result = execSync("bpmnlint --version", {
             encoding: "utf-8"
         });
 
         console.log(result);
-
-        const dirContents = fs.readdirSync(bpmnFiles, 'utf-8');
-        console.log(`Contents of ${bpmnFiles}:`, dirContents);
-
-        for (const file of dirContents) {
-            const filePath = path.join(bpmnFiles, file);
-            const fileContent = fs.readFileSync(filePath, 'utf-8');
-            console.log(`Content of ${file}:`, fileContent);
-        }
+        //
+        // const dirContents = fs.readdirSync(bpmnFiles, 'utf-8');
+        // console.log(`Contents of ${bpmnFiles}:`, dirContents);
+        //
+        // for (const file of dirContents) {
+        //     const filePath = path.join(bpmnFiles, file);
+        //     const fileContent = fs.readFileSync(filePath, 'utf-8');
+        //     console.log(`Content of ${file}:`, fileContent);
+        // }
 
     } catch (error) {
         setFailed((error as Error)?.message ?? "Unknown error");
