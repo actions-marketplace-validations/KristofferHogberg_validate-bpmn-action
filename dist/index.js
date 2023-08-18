7fee0a8cb9619a6bf42dbf55ad64cb0af089d85b
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-18 11:29:16 +0200

    test running script

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,83 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 822:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const fs = __importStar(__nccwpck_require__(147));
+const path = __importStar(__nccwpck_require__(17));
+const core_1 = __nccwpck_require__(186);
+const child_process_1 = __nccwpck_require__(81);
+async function run() {
+    var _a;
+    const customRules = (0, core_1.getInput)('custom-rules-folder');
+    const bpmnFiles = (0, core_1.getInput)('bpmn-files-path');
+    const bpmnlintrcPath = (0, core_1.getInput)('bpmnlintrc-path');
+    try {
+        // Copy custom rules
+        const customRulesPath = path.join('/opt/hostedtoolcache/node/16.20.1/x64/lib/node_modules/bpmnlint/rules');
+        const customRulesFiles = fs.readdirSync(customRules, 'utf-8');
+        for (const file of customRulesFiles) {
+            const sourceFilePath = path.join(customRules, file);
+            const targetFilePath = path.join(customRulesPath, file);
+            fs.copyFileSync(sourceFilePath, targetFilePath);
+            console.log(`Copied ${file} to ${customRulesPath}`);
+        }
+        // List all available rules
+        const availableRules = fs.readdirSync(customRulesPath);
+        console.log(`Available rules:`, availableRules);
+        // Read and create .bpmnlintrc configuration file
+        const bpmnlintrcContent = fs.readFileSync(bpmnlintrcPath, 'utf-8');
+        const currentDirBpmnlintrcPath = path.join(process.cwd(), '.bpmnlintrc');
+        fs.writeFileSync(currentDirBpmnlintrcPath, bpmnlintrcContent);
+        console.log(`.bpmnlintrc file created in current directory.`);
+        // Run BPMN validation and output result
+        const bpmnFilesPath = path.join(bpmnFiles, '*.bpmn');
+        const bpmnFilesList = fs.readdirSync(bpmnFilesPath, 'utf-8');
+        for (const file of bpmnFilesList) {
+            const filePath = path.join(bpmnFilesPath, file);
+            console.log(`Validating ${file}...`);
+            const lintResult = (0, child_process_1.execSync)(`npx bpmnlint lint ${filePath}`, {
+                encoding: "utf-8"
+            });
+            console.log(`Linting result for ${file}:`, lintResult);
+        }
+    }
+    catch (error) {
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
+    }
+}
+run();
+
+
+/***/ }),
+
 /***/ 351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -2730,6 +2807,14 @@ module.exports = require("assert");
 
 /***/ }),
 
+/***/ 81:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("child_process");
+
+/***/ }),
+
 /***/ 113:
 /***/ ((module) => {
 
@@ -2848,32 +2933,12 @@ module.exports = require("util");
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
-var exports = __webpack_exports__;
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __nccwpck_require__(186);
-async function run() {
-    var _a;
-    const customRules = (0, core_1.getInput)('custom-rules-folder');
-    const bpmnFiles = (0, core_1.getInput)('bpmn-files-path');
-    const bpmnlintrcPath = (0, core_1.getInput)('bpmnlintrc-path');
-    try {
-        console.log(customRules);
-        console.log(bpmnFiles);
-        console.log(bpmnlintrcPath);
-    }
-    catch (error) {
-        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : 'Unknown error');
-    }
-}
-run();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(822);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { getInput, setFailed } from '@actions/core';
+import { getInput, setFailed } from "@actions/core";
 import { execSync } from 'child_process';
 
 async function run() {
@@ -9,12 +9,41 @@ async function run() {
     const bpmnlintrcPath = getInput('bpmnlintrc-path');
 
     try {
-            console.log(customRules)
-            console.log(bpmnFiles)
-            console.log(bpmnlintrcPath)
+        // Copy custom rules
+        const customRulesPath = path.join('/opt/hostedtoolcache/node/16.20.1/x64/lib/node_modules/bpmnlint/rules');
+        const customRulesFiles = fs.readdirSync(customRules, 'utf-8');
 
+        for (const file of customRulesFiles) {
+            const sourceFilePath = path.join(customRules, file);
+            const targetFilePath = path.join(customRulesPath, file);
+            fs.copyFileSync(sourceFilePath, targetFilePath);
+            console.log(`Copied ${file} to ${customRulesPath}`);
+        }
+
+        // List all available rules
+        const availableRules = fs.readdirSync(customRulesPath);
+        console.log(`Available rules:`, availableRules);
+
+        // Read and create .bpmnlintrc configuration file
+        const bpmnlintrcContent = fs.readFileSync(bpmnlintrcPath, 'utf-8');
+        const currentDirBpmnlintrcPath = path.join(process.cwd(), '.bpmnlintrc');
+        fs.writeFileSync(currentDirBpmnlintrcPath, bpmnlintrcContent);
+        console.log(`.bpmnlintrc file created in current directory.`);
+
+        // Run BPMN validation and output result
+        const bpmnFilesPath = path.join(bpmnFiles, '*.bpmn');
+        const bpmnFilesList = fs.readdirSync(bpmnFilesPath, 'utf-8');
+
+        for (const file of bpmnFilesList) {
+            const filePath = path.join(bpmnFilesPath, file);
+            console.log(`Validating ${file}...`);
+            const lintResult = execSync(`npx bpmnlint lint ${filePath}`, {
+                encoding: "utf-8"
+            });
+            console.log(`Linting result for ${file}:`, lintResult);
+        }
     } catch (error) {
-        setFailed((error as Error)?.message ?? 'Unknown error');
+        setFailed((error as Error)?.message ?? "Unknown error");
     }
 }
 
