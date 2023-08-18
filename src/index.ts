ee619e69f6d0c2364535dabeb15215d23c64bc75
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-18 10:32:40 +0200

    install node and bpmnlint on vm

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,46 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { getInput, setFailed } from "@actions/core";
+import { getInput, setFailed } from '@actions/core';
 import { execSync } from 'child_process';
 
 async function run() {
-    const customRules = getInput("custom-rules-folder");
-    const bpmnFiles = getInput("bpmn-files-path");
-    const bpmnlintrc = getInput("bpmnlintrc-path");
+    const customRules = getInput('custom-rules-folder');
+    const bpmnFiles = getInput('bpmn-files-path');
+    const bpmnlintrc = getInput('bpmnlintrc-path');
 
     try {
-
         // CHECK BPMNLINT INSTALLATION
-        const bpmnlintVersion = execSync("npx bpmnlint --version", {
-            encoding: "utf-8"
-        });
-        console.log("bpmnlint version:", bpmnlintVersion);
-
-        const bpmnlintInstallationPath = path.dirname(require.resolve('bpmnlint'));
-
-        console.log("bpmnlint path:", bpmnlintInstallationPath);
-
-        const contents = fs.readdirSync(bpmnlintInstallationPath);
-        contents.forEach(item => {
-            console.log(item);
-        });
+        execSync('npm install -g bpmnlint', { stdio: 'inherit' });
 
         // READ AND CREATE .BPMNLINTRC
         const bpmnlintConfigPath = path.join(bpmnlintrc, '.bpmnlintrc');
         const bpmnlintrcContent = fs.readFileSync(bpmnlintConfigPath, 'utf-8');
 
-        console.log(`BPMNLINT CONTENT ${bpmnlintConfigPath}:`, bpmnlintrcContent);
-
+        // READ BPMN FILES
+        const models = fs.readdirSync(bpmnFiles, 'utf-8')
+            .filter(file => path.extname(file) === '.bpmn'); // Filter files by extension
 
         // Write bpmnlintrc content to each BPMN file folder
-        // for (const file of bpmnlintConfig) {
-        //     const folderPath = path.dirname(path.join(bpmnFiles, file));
-        //     const bpmnlintrcFilePath = path.join(folderPath, '.bpmnlintrc');
-        //     fs.writeFileSync(bpmnlintrcFilePath, bpmnlintrcContent);
-        //     console.log(folderPath)
-        //
-        //     console.log(`.bpmnlintrc file created in ${folderPath}.`);
-        // }
-
-
-        // READ AND PRINT BPMN MODELS
-        // const models = fs.readdirSync(bpmnFiles, 'utf-8')
-        //     .filter(file => path.extname(file) === '.bpmn'); // Filter files by extension
-        //
-        // console.log(`Contents of ${bpmnFiles}:`, models)
-        //
-        // for (const file of models) {
-        //     const filePath = path.join(bpmnFiles, file);
-        //     const fileContent = fs.readFileSync(filePath, 'utf-8');
-        //     console.log(`Content of ${file}:`, fileContent);
-        // }
-
+        for (const file of models) {
+            const folderPath = path.dirname(path.join(bpmnFiles, file));
+            const bpmnlintrcFilePath = path.join(folderPath, '.bpmnlintrc');
+            fs.writeFileSync(bpmnlintrcFilePath, bpmnlintrcContent);
+            console.log(`.bpmnlintrc file created in ${folderPath}.`);
+        }
 
         // LINT BPMN FILES USING .BPMNLINTRC
-        // for (const file of models) {
-        //     const filePath = path.join(bpmnFiles, file);
-        //
-        //     console.log(`Validating ${file}...`);
-        //     const lintResult = execSync(`npx bpmnlint lint ${filePath}`, {
-        //         encoding: "utf-8"
-        //     });
-        //     console.log(`Linting result for ${file}:`, lintResult);
-        // }
+        for (const file of models) {
+            const filePath = path.join(bpmnFiles, file);
 
+            console.log(`Validating ${file}...`);
+            const lintResult = execSync(`npx bpmnlint lint ${filePath}`, {
+                encoding: 'utf-8'
+            });
+            console.log(`Linting result for ${file}:`, lintResult);
+        }
 
     } catch (error) {
-        setFailed((error as Error)?.message ?? "Unknown error");
+        setFailed((error as Error)?.message ?? 'Unknown error');
     }
 }
 
